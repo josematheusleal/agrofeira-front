e1539ef936520d36a3c00a17ca6d242e0840784c
Matheus Leal <matheus010307@hotmail.com>
2026-04-07 01:41:49 -0300

    Add dynamic export to DetalhamentoFeiraPage

--- app/(dashboard)/feiras/detalhamento/page.tsx
+++ app/(dashboard)/feiras/detalhamento/page.tsx
@@ -176,6 +176,8 @@ function DetCard({
   );
 }
 
+export const dynamic = "force-dynamic";
+
 export default function DetalhamentoFeiraPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
